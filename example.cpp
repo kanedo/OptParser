4dd675c9b6b76cc9b7ed9ef017ec36e4479765f2
Gabriel Bretschner <software@kanedo.net>
2014-06-24 19:41:55 +0200

    added flags to example.cpp

--- example.cpp
+++ example.cpp
@@ -14,6 +14,7 @@ int main(int argc, char const *argv[])
 	options->setOption("required_option", "ro", "required-option", "This is a required option you must specify", true);
 	options->setOption("option_default", "d", "default-option", "This is an option with a default value", false, "default value");
 	options->setOption("option", "o", "option", "This is a normal option");
+	options->addFlag("flag", "f", "flag", "use flags to add binary values");
 	if(!options->parseOptions(argc, argv)){
 		return EXIT_FAILURE;
 	}
@@ -21,5 +22,9 @@ int main(int argc, char const *argv[])
 	string required_option = options->getValue("required_option");
 	string option_default  = options->getValue("option_default");	// returns "default value" if not specified
 	string option          = options->getValue("option");			// return an empty string if not specified
+	cout << required_option << endl;
+	if(options->isSet("flag")){
+		cout << "flag is set " << endl;
+	}
 	return 0;
 }

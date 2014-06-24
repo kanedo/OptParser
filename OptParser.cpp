2bbf0ec173c3d0bd1066cf7cfe7d4b37d2be26a2
Gabriel Bretschner <software@kanedo.net>
2014-06-24 10:39:16 +0200

    use namespace Kanedo

--- OptParser.cpp
+++ OptParser.cpp
@@ -1,182 +1,183 @@
 #include "OptParser.h"
-
-OptParser::OptParser(){
-	this->output	= &cout;
-	this->help		= "";
-	this->required	= vector<string>();
-	this->strict	= false;
-}
-
-OptParser::OptParser(ostream &output){
-	this->output	= &output;
-}
-
-void OptParser::setHelpText(string text){
-	this->help = text;
-}
-
-string OptParser::getHelpText(){
-	return  this->help;
-}
-
-string OptParser::getUsageText(){
-	string output = this->getHelpText();
-	output += "\n";
-	unordered_map<string, string> options_output;
-	size_t max_length = 0;
-	for(unordered_map<string, Option>::iterator option = this->options.begin(); option != this->options.end(); ++option){
-		string option_output = "";
-		option_output.append("\t");
-		option_output.append("-");
-		option_output.append(option->second.short_opt);
-		option_output.append(", ");
-		option_output.append("--");
-		option_output.append(option->second.long_opt);
-		options_output.insert(make_pair(option->second.name, option_output));
-		max_length = max(max_length, option_output.length());
+namespace Kanedo{
+	OptParser::OptParser(){
+		this->output	= &cout;
+		this->help		= "";
+		this->required	= vector<string>();
+		this->strict	= false;
 	}
 
-	for (unordered_map<string, string>::iterator option = options_output.begin(); option != options_output.end(); ++option) {
-		
-		output.append(option->second);
-		for (size_t i = 0; i < (max_length - option->second.length()) + 1; ++i) {
-			output.append(" ");
+	OptParser::OptParser(ostream &output){
+		this->output	= &output;
+	}
+
+	void OptParser::setHelpText(string text){
+		this->help = text;
+	}
+
+	string OptParser::getHelpText(){
+		return  this->help;
+	}
+
+	string OptParser::getUsageText(){
+		string output = this->getHelpText();
+		output += "\n";
+		unordered_map<string, string> options_output;
+		size_t max_length = 0;
+		for(unordered_map<string, Option>::iterator option = this->options.begin(); option != this->options.end(); ++option){
+			string option_output = "";
+			option_output.append("\t");
+			option_output.append("-");
+			option_output.append(option->second.short_opt);
+			option_output.append(", ");
+			option_output.append("--");
+			option_output.append(option->second.long_opt);
+			options_output.insert(make_pair(option->second.name, option_output));
+			max_length = max(max_length, option_output.length());
 		}
-		Option opt = this->options[option->first];
-		output.append(opt.name);
-		output.append(" ");
-		if(opt.required){
-			output.append("(required)");
-			output.append(" ");
-		}
-		output.append(opt.descr);
-		output.append(" ");
-		
-		if(opt.use_default){
-			output.append("default: ");
-			output.append(opt.default_val);
-		}
-		output.append("\n");
-	}
-	return output;
-}
 
-void OptParser::showUsage(){
-	*this->output << this->getUsageText();
-}
-
-
-void OptParser::setOption(string name, string short_opt, string long_opt, string descr, bool required, string default_val, bool use_default){
-	Option opt;
-	opt.name = name;
-	opt.short_opt = short_opt;
-	opt.long_opt = long_opt;
-	opt.descr = descr;
-	opt.required = required;
-	if (required) {
-		this->required.push_back(name);
-	}
-	opt.default_val = default_val;
-	opt.use_default = use_default;
-	if(use_default){
-		this->defaults.insert(make_pair(name, default_val));
-	}
-	this->options.insert(make_pair(name, opt));
-}
-
-void OptParser::setOption(string name, string short_opt, string long_opt, string descr){
-	this->setOption(name, short_opt, long_opt, descr, false, "", false);
-}
-
-void OptParser::setOption(string name, string short_opt, string long_opt, string descr, bool required){
-	this->setOption(name, short_opt, long_opt, descr, required, "", false);
-}
-
-void OptParser::setOption(string name, string short_opt, string long_opt, string descr, bool required, string default_val){
-	this->setOption(name, short_opt, long_opt, descr, required, default_val, true);
-}
-
-void OptParser::setDefaultValue(string name, string value){
-	if(this->options.find(name) != this->options.end()){
-		this->options[name].default_val = value;
-		this->options[name].use_default = true;
-		this->defaults.insert(make_pair(name, value));
-	}
-}
-
-std::unordered_map<string, OptParser::Option>::iterator OptParser::findOption(string argv){
-	if (argv.size() > 1) {
-		if (argv.substr(0, 1).compare("-") == 0) {
-			argv.erase(0, 1);
-			if(argv.substr(0, 1).compare("-") == 0){
-				// long option
-				argv.erase(0, 1);
-				for (std::unordered_map<string, OptParser::Option>::iterator option = this->options.begin(); option != this->options.end(); ++option) {
-					if(option->second.long_opt.compare(argv) == 0){
-						return option;
-					}
-				}
-			}else{
-				// short option
-				for (std::unordered_map<string, OptParser::Option>::iterator option = this->options.begin(); option != this->options.end(); ++option) {
-					if(option->second.short_opt.compare(argv) == 0){
-						return option;
-					}
-				}
+		for (unordered_map<string, string>::iterator option = options_output.begin(); option != options_output.end(); ++option) {
+			
+			output.append(option->second);
+			for (size_t i = 0; i < (max_length - option->second.length()) + 1; ++i) {
+				output.append(" ");
 			}
-		}
-	}
-	return this->options.end();
-}
-
-bool OptParser::parseOptions(int argc, char* argv[]){
-	if((argc-1)/2 < this->required.size()){
-		*this->output << "error: not enought options given" << endl;
-		this->showUsage();
-		return false;
-	}
-	
-	if(this->strict && (argc-1)/2 > this->options.size()){
-		*this->output << "error: strict usage on - too many options " << endl;
-		this->showUsage();
-		return false;
-	}
-	vector<string> required = this->required;
-	unordered_map<string, string> defaults = this->defaults;
-	for (int i = 0; i < argc-1; ++i) {
-		string arg_name = string(argv[i]);
-		std::unordered_map<string, OptParser::Option>::iterator opt_iterator = this->findOption(argv[i]);
-		if (opt_iterator != this->options.end()) {
-			Option opt = opt_iterator->second;
+			Option opt = this->options[option->first];
+			output.append(opt.name);
+			output.append(" ");
 			if(opt.required){
-				for (size_t i = 0; i < required.size(); ++i) {
-					if(opt.name.compare(required[i]) == 0){
-						required.erase(required.begin()+i);
-						break;
+				output.append("(required)");
+				output.append(" ");
+			}
+			output.append(opt.descr);
+			output.append(" ");
+			
+			if(opt.use_default){
+				output.append("default: ");
+				output.append(opt.default_val);
+			}
+			output.append("\n");
+		}
+		return output;
+	}
+
+	void OptParser::showUsage(){
+		*this->output << this->getUsageText();
+	}
+
+
+	void OptParser::setOption(string name, string short_opt, string long_opt, string descr, bool required, string default_val, bool use_default){
+		Option opt;
+		opt.name = name;
+		opt.short_opt = short_opt;
+		opt.long_opt = long_opt;
+		opt.descr = descr;
+		opt.required = required;
+		if (required) {
+			this->required.push_back(name);
+		}
+		opt.default_val = default_val;
+		opt.use_default = use_default;
+		if(use_default){
+			this->defaults.insert(make_pair(name, default_val));
+		}
+		this->options.insert(make_pair(name, opt));
+	}
+
+	void OptParser::setOption(string name, string short_opt, string long_opt, string descr){
+		this->setOption(name, short_opt, long_opt, descr, false, "", false);
+	}
+
+	void OptParser::setOption(string name, string short_opt, string long_opt, string descr, bool required){
+		this->setOption(name, short_opt, long_opt, descr, required, "", false);
+	}
+
+	void OptParser::setOption(string name, string short_opt, string long_opt, string descr, bool required, string default_val){
+		this->setOption(name, short_opt, long_opt, descr, required, default_val, true);
+	}
+
+	void OptParser::setDefaultValue(string name, string value){
+		if(this->options.find(name) != this->options.end()){
+			this->options[name].default_val = value;
+			this->options[name].use_default = true;
+			this->defaults.insert(make_pair(name, value));
+		}
+	}
+
+	std::unordered_map<string, OptParser::Option>::iterator OptParser::findOption(string argv){
+		if (argv.size() > 1) {
+			if (argv.substr(0, 1).compare("-") == 0) {
+				argv.erase(0, 1);
+				if(argv.substr(0, 1).compare("-") == 0){
+					// long option
+					argv.erase(0, 1);
+					for (std::unordered_map<string, OptParser::Option>::iterator option = this->options.begin(); option != this->options.end(); ++option) {
+						if(option->second.long_opt.compare(argv) == 0){
+							return option;
+						}
+					}
+				}else{
+					// short option
+					for (std::unordered_map<string, OptParser::Option>::iterator option = this->options.begin(); option != this->options.end(); ++option) {
+						if(option->second.short_opt.compare(argv) == 0){
+							return option;
+						}
 					}
 				}
 			}
-			this->values.insert(make_pair(arg_name, argv[++i]));
-			if(defaults.find(arg_name) != defaults.end()){
-				defaults.erase(arg_name);
+		}
+		return this->options.end();
+	}
+
+	bool OptParser::parseOptions(int argc, char* argv[]){
+		if((argc-1)/2 < this->required.size()){
+			*this->output << "error: not enought options given" << endl;
+			this->showUsage();
+			return false;
+		}
+		
+		if(this->strict && (argc-1)/2 > this->options.size()){
+			*this->output << "error: strict usage on - too many options " << endl;
+			this->showUsage();
+			return false;
+		}
+		vector<string> required = this->required;
+		unordered_map<string, string> defaults = this->defaults;
+		for (int i = 0; i < argc-1; ++i) {
+			string arg_name = string(argv[i]);
+			std::unordered_map<string, OptParser::Option>::iterator opt_iterator = this->findOption(argv[i]);
+			if (opt_iterator != this->options.end()) {
+				Option opt = opt_iterator->second;
+				if(opt.required){
+					for (size_t i = 0; i < required.size(); ++i) {
+						if(opt.name.compare(required[i]) == 0){
+							required.erase(required.begin()+i);
+							break;
+						}
+					}
+				}
+				this->values.insert(make_pair(arg_name, argv[++i]));
+				if(defaults.find(arg_name) != defaults.end()){
+					defaults.erase(arg_name);
+				}
 			}
 		}
-	}
-	if(required.size() > 0){
-		*this->output << "error: missing required options " << endl;
-		for(vector<string>::iterator req = required.begin(); req != required.end(); ++req){
-			*this->output << "\t" << *req << endl;
+		if(required.size() > 0){
+			*this->output << "error: missing required options " << endl;
+			for(vector<string>::iterator req = required.begin(); req != required.end(); ++req){
+				*this->output << "\t" << *req << endl;
+			}
+			this->showUsage();
+			return false;
 		}
-		this->showUsage();
-		return false;
+		for(unordered_map<string, string>::iterator opt = defaults.begin(); opt != defaults.end(); ++opt){
+			this->values.insert(*opt);
+		}
+		return true;
 	}
-	for(unordered_map<string, string>::iterator opt = defaults.begin(); opt != defaults.end(); ++opt){
-		this->values.insert(*opt);
-	}
-	return true;
-}
 
 
-string OptParser::getValue(string name){
-	return this->values[name];
+	string OptParser::getValue(string name){
+		return this->values[name];
+	}
 }
--- OptParser.h
+++ OptParser.h
@@ -5,108 +5,116 @@
 #include <unordered_map>
 #include <iostream>
 #include <algorithm>
-using namespace std;
-class OptParser {
-protected:
-	struct Option{
-		string name;
-		string short_opt;
-		string long_opt;
-		string descr;
-		bool required;
-		string default_val;
-		bool use_default;
-	};
-	unordered_map<string, string> values;
-	vector<string> required;
-	unordered_map<string, Option> options;
-	unordered_map<string, string> defaults;
-	string help;
-	void	setOption(string name, string short_opt, string long_opt, string descr, bool required, string default_val, bool use_default);
-	
-	bool strict;
-	string getUsageText();
-	ostream *output;
-	
-	unordered_map<string, Option>::iterator findOption(string argv);
-public:
-	OptParser();
-	/**
-	 * constructor
-	 * @param ostream& output define output stream for usage text. default std::cout
-	 */
-	OptParser(ostream &output);
-	
-	/**
-	 * define intro text.
-	 * usually a short description of what the application does
-	 * @param text the intro text
-	 */
-	void	setHelpText(string text);
-	/**
-	 * define an option
-	 * @param string name      the name of the option used in @see OptParser::getValue	
-	 * @param string short_opt the short variant of this option (w/o -)
-	 * @param string long_opt  the long variant of this option (w/o --)
-	 * @param string descr     a description of what this options is used for
-	 */
-	void	setOption(string name, string short_opt, string long_opt, string descr);
+namespace Kanedo{
+	using std::cout;
+	using std::endl;
+	using std::vector;
+	using std::unordered_map;
+	using std::max;
+	using std::string;
+	using std::ostream;
 
-	/**
-	 * define an option
-	 * @param string name      the name of the option used in @see OptParser::getValue	
-	 * @param string short_opt the short variant of this option (w/o -)
-	 * @param string long_opt  the long variant of this option (w/o --)
-	 * @param string descr     a description of what this options is used for
-	 * @param bool   required  set to true if this option is required
-	 */
-	void	setOption(string name, string short_opt, string long_opt, string descr, bool required);
-	/**
-	 * [setOption description]
-	 * @param string name        the name of the option used in @see OptParser::getValue
-	 * @param string short_opt   the short variant of this option (w/o -)
-	 * @param string long_opt    the long variant of this option (w/o --)
-	 * @param string descr       a description of what this options is used for
-	 * @param bool   required    set to true if this option is required
-	 * @param string default_val define a default value for this option (only makes sense if required is set to false)
-	 */
-	void	setOption(string name, string short_opt, string long_opt, string descr, bool required, string default_val);
+	class OptParser {
+	protected:
+		struct Option{
+			string name;
+			string short_opt;
+			string long_opt;
+			string descr;
+			bool required;
+			string default_val;
+			bool use_default;
+		};
+		unordered_map<string, string> values;
+		vector<string> required;
+		unordered_map<string, Option> options;
+		unordered_map<string, string> defaults;
+		string help;
+		void	setOption(string name, string short_opt, string long_opt, string descr, bool required, string default_val, bool use_default);
+		
+		bool strict;
+		string getUsageText();
+		ostream *output;
+		
+		unordered_map<string, Option>::iterator findOption(string argv);
+	public:
+		OptParser();
+		/**
+		 * constructor
+		 * @param ostream& output define output stream for usage text. default std::cout
+		 */
+		OptParser(ostream &output);
+		
+		/**
+		 * define intro text.
+		 * usually a short description of what the application does
+		 * @param text the intro text
+		 */
+		void	setHelpText(string text);
+		/**
+		 * define an option
+		 * @param string name      the name of the option used in @see OptParser::getValue	
+		 * @param string short_opt the short variant of this option (w/o -)
+		 * @param string long_opt  the long variant of this option (w/o --)
+		 * @param string descr     a description of what this options is used for
+		 */
+		void	setOption(string name, string short_opt, string long_opt, string descr);
 
-	/**
-	 * define default value for option later
-	 * @param string name  the name of the option
-	 * @param string value the default value
-	 */
-	void	setDefaultValue(string name, string value);
+		/**
+		 * define an option
+		 * @param string name      the name of the option used in @see OptParser::getValue	
+		 * @param string short_opt the short variant of this option (w/o -)
+		 * @param string long_opt  the long variant of this option (w/o --)
+		 * @param string descr     a description of what this options is used for
+		 * @param bool   required  set to true if this option is required
+		 */
+		void	setOption(string name, string short_opt, string long_opt, string descr, bool required);
+		/**
+		 * [setOption description]
+		 * @param string name        the name of the option used in @see OptParser::getValue
+		 * @param string short_opt   the short variant of this option (w/o -)
+		 * @param string long_opt    the long variant of this option (w/o --)
+		 * @param string descr       a description of what this options is used for
+		 * @param bool   required    set to true if this option is required
+		 * @param string default_val define a default value for this option (only makes sense if required is set to false)
+		 */
+		void	setOption(string name, string short_opt, string long_opt, string descr, bool required, string default_val);
 
-	/**
-	 * returns the value of given option. 
-	 * If option does not exists it'll return an empty string
-	 * If option is not set an has a default value the default is used
-	 * @param  string name name of the option
-	 * @return string
-	 */
-	string	getValue(string name);
-	
-	/**
-	 * return the Help text
-	 * @return string
-	 */
-	string	getHelpText();
+		/**
+		 * define default value for option later
+		 * @param string name  the name of the option
+		 * @param string value the default value
+		 */
+		void	setDefaultValue(string name, string value);
 
-	/**
-	 * show the usage text to stream defined in @see OptParser::OptParser 
-	 * default is std::cout
-	 */
-	void	showUsage();
+		/**
+		 * returns the value of given option. 
+		 * If option does not exists it'll return an empty string
+		 * If option is not set an has a default value the default is used
+		 * @param  string name name of the option
+		 * @return string
+		 */
+		string	getValue(string name);
+		
+		/**
+		 * return the Help text
+		 * @return string
+		 */
+		string	getHelpText();
 
-	/**
-	 * parse the options (usually parameters of main()).
-	 * @param  int    argc the size of argv
-	 * @param  char** argv parameters array
-	 * @return bool        true if parsing went well, false otherwise. Usage is shown automatically
-	 */
-	bool	parseOptions(int argc, char* argv[]);
-	
-};
+		/**
+		 * show the usage text to stream defined in @see OptParser::OptParser 
+		 * default is std::cout
+		 */
+		void	showUsage();
+
+		/**
+		 * parse the options (usually parameters of main()).
+		 * @param  int    argc the size of argv
+		 * @param  char** argv parameters array
+		 * @return bool        true if parsing went well, false otherwise. Usage is shown automatically
+		 */
+		bool	parseOptions(int argc, char* argv[]);
+		
+	};}
 #endif
